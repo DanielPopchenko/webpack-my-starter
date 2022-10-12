c8a7a30fe7ef3b6e194b01f29b1c91becdb8e27e
Daniel Popchenko <104201822+DanielPopchenko@users.noreply.github.com>
2022-10-12 14:12:44 +0300

    creating basics starter page

--- src/js/a.js
+++ src/js/a.js
@@ -1,4 +1,12 @@
 const add = (a, b) => a + b;
 add(10, 25);
 
+// const imgWrapper = document.querySelector('.img-wrapper');
+// const imgRef = '../img.png';
+// const imgEl = document.createElement('img');
+// imgEl.src = imgRef;
+// imgEl.classList.add('img');
+// console.log(imgEl);
+// imgWrapper.insertAdjacentElement('beforeend', imgEl);
+
 export default 5;
--- webpack.config.js
+++ webpack.config.js
@@ -3,35 +3,44 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        /**
-         * __dirname берет весть путь (абсолютный путь)
-         * и добавляет папку build
-         */
-        path: path.resolve(__dirname, 'build'),
-        // Изменили имя файла в который мы выкидываем код
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [
-            { test: /\.m?js$/, exclude: /node_modules/, use: 'babel-loader' },
-            // Бери файлы css и используй для них css-loader
-            {
-                test: /\.s[ac]ss$/i,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
-            },
-        ],
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'styles.css',
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-        }),
+  entry: './src/index.js',
+  output: {
+    /**
+     * __dirname берет весть путь (абсолютный путь)
+     * и добавляет папку build
+     */
+    path: path.resolve(__dirname, 'build'),
+    // Изменили имя файла в который мы выкидываем код
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      { test: /\.m?js$/, exclude: /node_modules/, use: 'babel-loader' },
+      // Бери файлы css и используй для них css-loader
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+        loader: 'file-loader',
+        options: {
+          publicPath: '../',
+          name: `[name].[ext]`,
+        },
+      },
     ],
-    devServer: {
-        port: 1234,
-    },
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    port: 1234,
+  },
 };
